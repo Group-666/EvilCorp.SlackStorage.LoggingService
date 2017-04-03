34fd4f2646f0a95a1dd0f6d6bf832c3047e03001
Martin Erik Bisgaard <mebisgaard@gmail.com>
2017-04-03 15:25:15 +0200

    Changed they way the parser handles the log level.

--- LoggingService/EvilCorp.SlackStorage.LoggingService.DomainTypes/LogEntryParser.cs
+++ LoggingService/EvilCorp.SlackStorage.LoggingService.DomainTypes/LogEntryParser.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace EvilCorp.SlackStorage.LoggingService.DomainTypes
 {
@@ -9,14 +7,14 @@ namespace EvilCorp.SlackStorage.LoggingService.DomainTypes
     {
         public static LogEntry Parse(JObject json)
         {
-            var component = (string) json["component"] ?? throw new ArgumentException("The component is not found in json object.");
-            var message = (string) json["message"] ?? throw new ArgumentException("The message is not found in json object.");
-            var level = json["level"] ?? throw new ArgumentException("The level is found in json object.");
+            var component = (string)json["component"] ?? throw new ArgumentException("The component is not found in json object.");
+            var message = (string)json["message"] ?? throw new ArgumentException("The message is not found in json object.");
+            var level = (int?)json["level"] ?? throw new ArgumentException("The level is found in json object.");
 
-            if ((int)level < 1 || (int)level > 5)
+            if (level < 1 || level > 5)
                 throw new ArgumentException("The log level is not supported.");
 
-            return new LogEntry(component, message, (LogLevel)(int)level);
+            return new LogEntry(component, message, (LogLevel)level);
         }
     }
 }
